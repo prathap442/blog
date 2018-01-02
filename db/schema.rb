a5106759876190c9b244fc391ac03f934c46c3ba
prathap442 <pmohansaikrishna@gmail.com>
2018-01-02 14:36:10 +0530

    commit3

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102082929) do
+ActiveRecord::Schema.define(version: 20180102084437) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
