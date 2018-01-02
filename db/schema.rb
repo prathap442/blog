19b5f709600fd33f1539e47bcbb58dfda9e9a76b
prathap442 <pmohansaikrishna@gmail.com>
2018-01-02 14:49:23 +0530

    scaffolded the article

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102082929) do
+ActiveRecord::Schema.define(version: 20180102091706) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
